cb7111fccc0f6a572108062882362dcb7d27c481
Jakub Šárník <jakub.sarnik@gmail.com>
2022-12-10 18:45:30 +0100

    Update helpers

--- Helpers.hs
+++ Helpers.hs
@@ -1,7 +1,20 @@
 module Helpers
-  (listToPair)
+  (listToPair, slidingWindow, readLines)
 where
 
 listToPair :: [a] -> (a, a)
 listToPair [x, y] = (x, y)
 listToPair _      = error "listToPair: not a pair list"
+
+slidingWindow :: Int -> [a] -> [[a]]
+slidingWindow _ [] = []
+slidingWindow n xs =
+  if length peek < n then
+    []
+  else
+    peek : slidingWindow n (drop 1 xs) 
+  where
+    peek = take n xs
+
+readLines :: FilePath -> IO [String]
+readLines = fmap lines . readFile 
